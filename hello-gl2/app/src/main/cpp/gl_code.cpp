7f7b8825ad6491669cf4389daf04b3bbf3cd1a98
vilyLei <vily313@126.com>
2022-01-12 10:19:54 +0800

    fix: adjust targetSdkVersion to 29 for the lastest SDK.

--- hello-gl2/app/src/main/cpp/gl_code.cpp
+++ hello-gl2/app/src/main/cpp/gl_code.cpp
@@ -51,7 +51,7 @@ auto gVertexShader =
 auto gFragmentShader =
     "precision mediump float;\n"
     "void main() {\n"
-    "  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+    "  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);\n"
     "}\n";
 
 GLuint loadShader(GLenum shaderType, const char* pSource) {
